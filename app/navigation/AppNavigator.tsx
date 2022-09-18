755bb0037b595bb5716799f103cd3ee401ddd510
Aman Srivastava <aman.srivastava@ticketmaster.com>
2022-09-18 22:04:38 +0530

    Styling the Tabs navigator

--- app/navigation/AppNavigator.tsx
+++ app/navigation/AppNavigator.tsx
@@ -3,15 +3,32 @@ import { AppStateTabList } from '../config/utils';
 import ListingDetailsScreen from '../screens/ListingDetailsScreen';
 import AccountNavigator from './AccountNavigator';
 import FeedNavigator from './FeedNavigator';
+import {MaterialCommunityIcons} from '@expo/vector-icons';
+import NewListingButton from './NewListingButton';
+import ListingEditScreen from '../screens/ListingEditScreen';
 
 const Tab = createBottomTabNavigator<AppStateTabList>();
+
+const FeedTabOptions = () => {
+    return {
+        ...GeneralTabOptions,
+        headerShown: false,
+    }
+}
+
+const GeneralTabOptions = (name: any) => {
+    return {
+        tabBarIcon: ({size, color}: any) => <MaterialCommunityIcons size={size} color={color} name={name}/>
+    }
+}
 
 const AppNavigator = () => {
     return (
         <Tab.Navigator>
-            <Tab.Screen  options={{headerShown: false}} name='Feed' component={FeedNavigator}/>
-            <Tab.Screen name='ListingEdit' component={ListingDetailsScreen}/>
-            <Tab.Screen name='Account' component={AccountNavigator}/>
+            <Tab.Screen options={() => GeneralTabOptions("home")} name='Feed' component={FeedNavigator}/>
+            <Tab.Screen options={({navigation}: any) => ({tabBarButton: () => <NewListingButton onPress={() => navigation.navigate("ListingEdit")}/>})} 
+            name='ListingEdit' component={ListingEditScreen}/>
+            <Tab.Screen options={() => GeneralTabOptions("account")} name='Account' component={AccountNavigator}/>
         </Tab.Navigator>
     )
 };
